3595d57e2249fc2f70d81664e56537a7b963b70a
exepta <genshin.exepta@outlook.com>
2025-02-02 14:35:08 +0100

    feat(MOD-9): audio management is now over the service

---
+++ src/audio/audio.rs
@@ -0,0 +1,16 @@
+use bevy::prelude::*;
+use bevy_kira_audio::{DynamicAudioChannels};
+use crate::audio::{AudioManager, AudioType};
+use crate::manager::GameState;
+
+pub struct AudioHandlerPlugin;
+
+impl Plugin for AudioHandlerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(GameState::InGame), setup);
+    }
+}
+
+fn setup(asset_server: Res<AssetServer>, mut audio: ResMut<DynamicAudioChannels>, mut audio_manager: ResMut<AudioManager>) {
+    audio_manager.add_audio("test", AudioType::Environment, "audio/env_test.ogg", &mut audio, &asset_server);
+}
--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -1,36 +1,92 @@
+mod audio;
+
 use std::time::Duration;
 use bevy::prelude::*;
 use bevy::utils::HashMap;
-use bevy_kira_audio::{AudioControl, AudioEasing, AudioInstance, AudioTween, DynamicAudioChannels};
-use crate::manager::GameState;
+use bevy_kira_audio::*;
+use crate::audio::audio::AudioHandlerPlugin;
 
 pub struct AudioStorePlugin;
 
 impl Plugin for AudioStorePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GameState::InGame), setup);
+        app.insert_resource(AudioManager::new());
+        app.add_plugins(AudioHandlerPlugin);
     }
 }
 
-#[derive(Resource, Debug)]
+#[derive(Debug, Default, Clone, Eq, PartialEq, Hash)]
+#[allow(dead_code)]
+pub enum AudioType {
+    Environment,
+    Battle,
+    Sfx,
+    Ui,
+    Character,
+    #[default]
+    Unknown
+}
+
+#[derive(Resource)]
+#[allow(dead_code)]
 pub struct AudioManager {
-    pub volume: f32,
+    pub master_volume: f32,
+    pub ui_volume: f32,
+    pub sfx_volume: f32,
+    pub character_voice_volume: f32,
+    pub environment_volume: f32,
     pub muted: bool,
-    pub channels: HashMap<String, Handle<AudioInstance>>,
+    pub audio: HashMap<String, AudioType>
+}
+
+impl AudioManager {
+    pub fn new() -> Self {
+        Self {
+            master_volume: 1.0,
+            ui_volume: 0.2,
+            sfx_volume: 0.1,
+            character_voice_volume: 0.1,
+            environment_volume: 0.05,
+            muted: false,
+            audio: HashMap::new()
+        }
+    }
+
+    pub fn add_audio(&mut self,
+                     channel_name: &str,
+                     audio_type: AudioType,
+                     track_path: &str,
+                     audio_kira_handle: &mut DynamicAudioChannels,
+                     asset_server: &AssetServer
+    ) {
+        if self.audio.contains_key(channel_name) {
+            warn!("Audio already in use: {}", channel_name);
+            return;
+        }
+
+        let looped;
+        match audio_type {
+            AudioType::Environment => { looped = true; }
+            AudioType::Battle => { looped = true; }
+            AudioType::Sfx => { looped = false; }
+            AudioType::Ui => { looped = false; }
+            AudioType::Character => { looped = false; }
+            AudioType::Unknown => { looped = false; }
+        }
+        let mut binding = audio_kira_handle.create_channel(channel_name)
+            .play(asset_server.load(track_path));
+        let build = binding
+            .fade_in(AudioTween::new(Duration::from_millis(2000), AudioEasing::OutPowi(2)))
+            .with_volume(0.05);
+        if looped { build.looped(); }
+
+        self.audio.insert(channel_name.to_string(), audio_type);
+    }
 }
 
 #[derive(Component, Resource, Debug)]
 pub struct EnvironmentAudio {
     pub base_track: String,
     pub battle_track: String,
-}
-
-fn setup(asset_server: Res<AssetServer>, mut audio: ResMut<DynamicAudioChannels>) {
-    let handle = asset_server.load("audio/env_test.ogg");
-    let audio_handle = audio.create_channel("test")
-        .play(handle)
-        .fade_in(AudioTween::new(Duration::from_millis(2000), AudioEasing::OutPowi(2)))
-        .with_volume(0.05)
-        .looped().handle();
 }
 
