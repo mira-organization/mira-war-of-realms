4ffb4155bdc4fe51732d6eac6f5bb6a9f55199c2
exepta <genshin.exepta@outlook.com>
2025-02-20 22:44:24 +0100

    fix(MOD-72): remove unused env and only allow line commit #5

--- post_build.sh
+++ post_build.sh
@@ -2,7 +2,9 @@
 mkdir -p libs
 
 # search for the current environment which store the cargo target folder
-TARGET_DIR="${CARGO_TARGET_DIR:-target/release}"
+echo "Search folder: $1"
+echo "Folder content:"
+ls -R "$1"
 
 # fetch the .so or .dylib from the target folder and store it to libs
-find "$TARGET_DIR" -maxdepth 1 -type f \( -name "*.so" -o -name "*.dylib" \) -exec mv {} libs/ \;
+find "$1" -maxdepth 1 -type f \( -name "*.so" -o -name "*.dylib" \) -exec mv {} libs/ \;
